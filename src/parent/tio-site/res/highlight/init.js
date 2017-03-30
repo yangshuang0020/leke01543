07ed8090695f1297cac08c3b513f687c8e93c604
talent-tan <tywo45@163.com>
2017-03-30 20:48:20 +0800

    init

--- src/parent/tio-site/res/highlight/init.js
+++ src/parent/tio-site/res/highlight/init.js
@@ -1,9 +1,8 @@
 var nav_list_conf=[
 {href:"./",innerHTML:"首 页"},
 {href:"./quickstart.html",innerHTML:"30分钟快乐入门"},
-{href:"./releases.html",innerHTML:"版本发布列表"},
 {href:"./",innerHTML:"API文档(敬请期待)"},
-{href:"https://git.oschina.net/tywo45/t-io",innerHTML:"下 载",target:"_blank"}
+{href:"https://git.oschina.net/tywo45/t-io",innerHTML:"@码云",target:"_blank"}
 ];
 
 function initnavlist(){
