8249f4e9d32deda606856d569e5ca5aeec7478b1
talent-tan <tywo45@163.com>
2017-04-30 07:28:26 +0800

    准备发新版本1.7.0

--- src/example/im-syn/client/installAndCopy.bat
+++
@@ -1,5 +0,0 @@
-call mvn clean install
-
-call rd ..\..\..\..\dist\examples\im-syn\client /s /q
-call xcopy target\dist\tio-examples-im-syn-client-1.7.0.v20170501-RELEASE ..\..\..\..\dist\examples\im-syn\client\ /s /e /q /y
-
--- src/example/im-syn/server/installAndCopy.bat
+++
@@ -1,5 +0,0 @@
-call mvn clean install
-
-call rd ..\..\..\..\dist\examples\im-syn\server /s /q
-call xcopy target\dist\tio-examples-im-syn-server-1.7.0.v20170501-RELEASE ..\..\..\..\dist\examples\im-syn\server\ /s /e /q /y
-
