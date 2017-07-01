9539fba10c2964a3f25405592941d4d1bb96d4ed
talent-tan <tywo45@163.com>
2017-07-01 17:05:59 +0800

    no commit message

--- src/zoo/websocket/demo/websocketserver-1/startup.bat
+++ src/zoo/websocket/demo/websocketserver-1/startup.bat
@@ -2,7 +2,7 @@ rem -Xms64m -Xmx2048m
 
 @echo off
 setlocal & pushd
-set APP_ENTRY=org.tio.http.server.demo1.HttpServerDemo1Starter
+set APP_ENTRY=org.tio.http.server.demo1.WsServerDemo1Starter
 set BASE=%~dp0
 set CP=%BASE%\config;%BASE%\lib\*
 java -Xverify:none -XX:+HeapDumpOnOutOfMemoryError -Dtio.default.read.buffer.size=512 -XX:HeapDumpPath=c:/java-t-io-im-server-pid.hprof -cp "%CP%" %APP_ENTRY%
