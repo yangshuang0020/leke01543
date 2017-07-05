4a7403aa070aff8772fb7f41e2f7fe072b9e3d59
talent-tan <tywo45@163.com>
2017-07-05 11:54:57 +0800

    1.7.2

--- src/example/im/client/installAndCopy.bat
+++ src/example/im/client/installAndCopy.bat
@@ -1,5 +1,5 @@
 call mvn clean install
 
 call rd ..\..\..\..\dist\examples\im\client /s /q
-call xcopy target\dist\tio-examples-im-client-1.7.1.v20170703-RELEASE ..\..\..\..\dist\examples\im\client\ /s /e /q /y
+call xcopy target\dist\tio-examples-im-client-1.7.2.v20170705-RELEASE ..\..\..\..\dist\examples\im\client\ /s /e /q /y
 
